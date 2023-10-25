a6c2a037f2b124c0c6ede47f17e958b6aa12de2b
Lili Zheng <fjzll.nju@gmail.com>
2023-10-25 21:10:44 +0800

    finish assignment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 
 from happy import Happy
 from sad import Sad
+from angry import Angry
 
 if __name__ == '__main__':
     # This is only needed if you have not deleted sense_hat.py
@@ -14,7 +15,7 @@ if __name__ == '__main__':
     ############################################################
 
     # Create a happy smiley, which is a subclass of Smiley
-    smiley = Happy()
+    smiley = Angry()
 
     # This is a form of #polymorphism, as the Happy smiley class
     # does not have a method called .show(). This means that
@@ -30,4 +31,4 @@ if __name__ == '__main__':
     # The method blink is implemented by the Happy class, but
     # is defined as an interface (i.e., an abstract base class
     # with an abstract method).
-    smiley.blink()
+    #smiley.blink()
